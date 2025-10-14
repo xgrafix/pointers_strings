3988a9e75e94643c8dd8a55561d31d6a5b3bbbcf
Ali Mohamed <xgrafix@gmail.com>
2025-10-14 15:25:50 +0300

    Clean up of the code

--- main.c
+++ main.c
@@ -4,27 +4,16 @@
 
 int main() {
 
-	//const char *c = "hello";
 
-	//const char **cp = &c;
-
-	//for (int i = 0; i < strlen(c); ++i) 
-	//	printf("%c ", *(c + i));
-
-	//printf("\n");
-
-	char *namesArray[] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "November", "December" };
+	char *namesArray[] = {"January", "February", "March", 
+				"April", "May", "June", "July", 
+				"August", "September", "November", "December" };
 
 	size_t arrTotalBytes = sizeof(namesArray);
 
 	printf("Total bytes occupied by namesArray: %zu\n\n", arrTotalBytes);
 
 	for(int i = 0; i < sizeof(namesArray) / 8; ++i) {
-
-		//printf("Size of [%s] = %ld \n", *(namesArray + i), strlen(*(namesArray + i)));
-		
-		//printf("%s \n", (namesArray[i]));  
-
 		printf("Size of [");
 
 		char* temp = namesArray[i];
